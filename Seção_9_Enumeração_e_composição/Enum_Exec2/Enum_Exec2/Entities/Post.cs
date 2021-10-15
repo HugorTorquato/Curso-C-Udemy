924e35666e662c24d4a8283b3831ef96d3730c03
Hugo Torquato <hugortorquato@gmail.com>
2021-10-14 22:00:55 -0300

    Enum_exec2

---
+++ Seção_9_Enumeração_e_composição/Enum_Exec2/Enum_Exec2/Entities/Post.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enum_Exec2.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        // - Declarar uma lista de comentários, do tipo da classe comment
+        // - Important instanciar essa lista para ela n ter chance de ser null, por isso 
+        // o "= new List<Comment>()" no final 
+
+        // A ideia é criar uma lista que pode relacionar a classe Posts a vários comentários
+        // por isso a criação da lista 
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        // Construtores
+
+        public Post()
+        {
+
+        }
+
+        // O atributo da lista vai ser desmarcado, não é passado ao construtor
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        // Colocar os operadores de adicionar e remover comentários
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment); // Usa o atributo Comments e não a classe Comment
+        }
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        // Implementar o ToString para definir como ele vai ser definido para string na saida
+        public override string ToString()
+        {
+            // String builder -> monta uma string de forma eficiente e retorna o resultado tipo string
+            // Ajuda no caso de ter muitos objetos ligados a classe... e no caso não tem de fazer um por um
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(Title);
+            sb.AppendLine(Likes + " Likes - " + Moment);
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+
+            // Parte dos comentários - Percorrer a lista de comentários
+            foreach (Comment Com in Comments)
+            {
+                sb.AppendLine(Com.Text);
+            }
+
+            return sb.ToString();                
+        }
+    }
+}
---
+++ Seção_9_Enumeração_e_composição/Enum_Exec2/Enum_Exec2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Enum_Exec2.Entities;
+
+namespace Enum_Exec2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciar os comentários primeiro
+
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow, that is awesome!");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zeland",
+                "I'm going to visit this wanterfull coountry",
+                12);
+
+            // Adicionar os comentáros na lista
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+
+            Comment c3 = new Comment("Good Night");
+            Comment c4 = new Comment("May the Force be with you");
+            
+            Post p2 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Good Night Guys",
+                "See you tomorrow",
+                5);
+
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+        }
+    }
+}
